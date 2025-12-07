39e81cf0a07cee0afe4cbeabf4c5924039f9f46d
protapkrghosh <protapg518@gmail.com>
2025-12-07 14:56:55 +0600

    add booking

--- src/modules/booking/booking.controller.ts
+++ src/modules/booking/booking.controller.ts
@@ -2,22 +2,22 @@ import { Request, Response } from "express";
 import { bookingServices } from "./booking.service";
 
 const createBooking = async (req: Request, res: Response) => {
-  try {
-    const result = await bookingServices.createBooking(req.body);
+   try {
+      const result = await bookingServices.createBooking(req.body);
 
-    res.status(201).json({
-      success: true,
-      message: "Booking created successfully",
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(400).json({
-      success: false,
-      message: error.message || "Failed to create booking",
-    });
-  }
+      res.status(201).json({
+         success: true,
+         message: "Booking created successfully",
+         data: result,
+      });
+   } catch (error: any) {
+      res.status(400).json({
+         success: false,
+         message: error.message || "Failed to create booking",
+      });
+   }
 };
 
 export const bookingControllers = {
-  createBooking,
+   createBooking,
 };
